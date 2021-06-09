d5b1e2cbbbb9c79d968c27b19c7c4060d5b71081
Cunknown <PLPlanguage@hotmail.com>
2021-06-09 03:12:33 -0700

    Released on 2019

---
+++ Linux/CompilerPLP/install.sh
@@ -0,0 +1,50 @@
+pkgin update
+pkg update
+brew update
+sudo apt-get update
+apt-get update
+yum update
+pacman -Syyu
+pkgin in binutils
+pkg install nano
+brew install nano
+yum install nano
+pacman -S nano
+apt-get install nano
+pkgin in nano
+pkgin in clang
+apt-get install clang
+yum install clang
+pacman -S clang
+pkg install clang
+pkg install gcc
+pkgin in dialog
+yum install dialog
+pacman -S dialog
+apt-get install dialog
+pkg install dialog
+pkgin in curl
+yum install curl
+apt-get install curl
+pacman -S curl
+pkg install curl
+apt-get install arecord
+apt-get install arecord
+yum install arecord
+pacman -S arecord
+pkg install arecord
+apt-get install sox
+yum install sox
+pacman -S sox
+pkg install sox
+apt-get install espeak
+yum install espeak
+pacman -S espeak
+pkg install espeak
+apt-get install mplayer
+yum install mplayer
+pacman -S mplayer
+pkg install mplayer
+clang++ Compile.cpp
+mv a.out Compiler.out
+./Compiler.out
